9b84da33b1f781bf5b9d4d7eba405bdcd183acf7
khameleonstorm <khameleonstorm@gmail.com>
2024-03-30 10:01:08 +0100

    yh

--- components/Contact.js
+++ components/Contact.js
@@ -4,7 +4,7 @@ import styles from "./Contact.module.css"
 export default function Contact() {
   return (
     <div className={styles.container}>
-      <a href="https://wa.me/message/OKQAJVXJ4ECWE1">
+      <a href="http://wa.me/17326319035">
         <div className={styles.btn}>Contact</div>
       </a>
     </div>
--- components/WhatsAppBtn.js
+++ components/WhatsAppBtn.js
@@ -4,7 +4,7 @@ import { IoLogoWhatsapp } from "react-icons/io"
 export default function WhatsAppBtn() {
   return (
     <div className={s.ctn}>
-      <a href='https://wa.me/message/OKQAJVXJ4ECWE1'>
+      <a href='http://wa.me/17326319035'>
         <IoLogoWhatsapp />
         <p>Contact Us</p>
       </a>
